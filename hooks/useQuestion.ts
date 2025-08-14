9023539eb7295d744302b5473c4b1dbaf76f4b64
TrongHuy0104 <tronghuy0104@gmail.com>
2025-08-14 16:08:22 +0700

    fix: not load paging data i get lesson questions

--- hooks/useQuestion.ts
+++ hooks/useQuestion.ts
@@ -1,23 +1,33 @@
-import { createQuestion, deleteQuestion, getLessonQuestions, getMyQuestions, getQuestionById, updateQuestion } from "@/api/question";
-import { useAuth } from "@/contexts/auth.context";
-import { Question } from "@/types/models/question.model";
-import { CreateQuestionRequest } from "@/types/requests/create-question.request";
-import { GetQuestionsRequest } from "@/types/requests/get-questions-request.model";
-import { UpdateQuestionRequest } from "@/types/requests/update-question.request";
-import { BaseResponse } from "@/types/responses/base.response";
-import { EntityListResponse } from "@/types/responses/entity-list-response.model";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { AxiosResponse } from "axios";
-import { useToast } from "./useToast";
+import {
+    createQuestion,
+    deleteQuestion,
+    getLessonQuestions,
+    getMyQuestions,
+    getQuestionById,
+    updateQuestion,
+} from '@/api/question';
+import { useAuth } from '@/contexts/auth.context';
+import { Question } from '@/types/models/question.model';
+import { CreateQuestionRequest } from '@/types/requests/create-question.request';
+import { GetQuestionsRequest } from '@/types/requests/get-questions-request.model';
+import { UpdateQuestionRequest } from '@/types/requests/update-question.request';
+import { BaseResponse } from '@/types/responses/base.response';
+import { EntityListResponse } from '@/types/responses/entity-list-response.model';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { useToast } from './useToast';
 
-export const useLessonQuestions = (materialId: string, request: GetQuestionsRequest) => {
+export const useLessonQuestions = (
+    materialId: string,
+    request: GetQuestionsRequest
+) => {
     const { isSignout, user } = useAuth();
-    
+
     const { data, isPending, error, refetch } = useQuery<
         AxiosResponse<BaseResponse<EntityListResponse<Question>>>,
         Error
     >({
-        queryKey: ['lesson-questions', materialId],
+        queryKey: ['lesson-questions', request],
         queryFn: () => {
             return getLessonQuestions(materialId, request);
         },
@@ -39,7 +49,7 @@ export const useLessonQuestions = (materialId: string, request: GetQuestionsRequ
 
 export const useMyQuestions = (request: GetQuestionsRequest) => {
     const { isSignout, user } = useAuth();
-    
+
     const { data, isPending, error, refetch } = useQuery<
         AxiosResponse<BaseResponse<EntityListResponse<Question>>>,
         Error
@@ -64,9 +74,12 @@ export const useMyQuestions = (request: GetQuestionsRequest) => {
     };
 };
 
-export const useQuestionById = (questionId: string, enabled: boolean = true) => {
+export const useQuestionById = (
+    questionId: string,
+    enabled: boolean = true
+) => {
     const { isSignout, user } = useAuth();
-    
+
     const { data, isPending, error, refetch } = useQuery<
         AxiosResponse<BaseResponse<Question>>,
         Error
@@ -91,10 +104,9 @@ export const useQuestionById = (questionId: string, enabled: boolean = true) => 
 };
 
 export const useCreateQuestion = () => {
-    
     const queryClient = useQueryClient();
     const toast = useToast();
-   
+
     return useMutation<
         AxiosResponse<BaseResponse<Question>>,
         Error,
@@ -118,56 +130,52 @@ export const useCreateQuestion = () => {
 };
 
 export const useUpdateQuestion = () => {
-    
     const queryClient = useQueryClient();
     const toast = useToast();
-    
+
     return useMutation<
-    AxiosResponse<BaseResponse<Question>>,
-    Error,
-    { questionId: string; data: UpdateQuestionRequest }
->({
-    mutationFn: ({ questionId, data }) => {
-        return updateQuestion(questionId, data);
-    },
-    onSuccess: (data) => {
-        queryClient.invalidateQueries({ queryKey: ['lesson-questions'] });
-        queryClient.invalidateQueries({ queryKey: ['my-questions'] });
-        queryClient.invalidateQueries({ queryKey: ['question', data.data.data?.id ?? ''] });
-        toast.success(
-            'Cập nhật câu hỏi thành công',
-            'Câu hỏi của bạn đã được cập nhật thành công'
-        );
-    },
-    onError: () => {
-        toast.errorGeneral();
-    },
-});
+        AxiosResponse<BaseResponse<Question>>,
+        Error,
+        { questionId: string; data: UpdateQuestionRequest }
+    >({
+        mutationFn: ({ questionId, data }) => {
+            return updateQuestion(questionId, data);
+        },
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ['lesson-questions'] });
+            queryClient.invalidateQueries({ queryKey: ['my-questions'] });
+            queryClient.invalidateQueries({
+                queryKey: ['question', data.data.data?.id ?? ''],
+            });
+            toast.success(
+                'Cập nhật câu hỏi thành công',
+                'Câu hỏi của bạn đã được cập nhật thành công'
+            );
+        },
+        onError: () => {
+            toast.errorGeneral();
+        },
+    });
 };
 
 export const useDeleteQuestion = () => {
-    
     const queryClient = useQueryClient();
     const toast = useToast();
-    
-    return useMutation<
-    AxiosResponse<BaseResponse<Question>>,
-    Error,
-    string
->({ 
-    mutationFn: (questionId: string) => {
-        return deleteQuestion(questionId);
-    },
-    onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ['lesson-questions'] });
-        queryClient.invalidateQueries({ queryKey: ['my-questions'] });
-        toast.success(
-            'Xóa câu hỏi thành công',
-            'Câu hỏi của bạn đã được xóa thành công'
-        );
-    },
-    onError: () => {
-        toast.errorGeneral();
-    },
-});
+
+    return useMutation<AxiosResponse<BaseResponse<Question>>, Error, string>({
+        mutationFn: (questionId: string) => {
+            return deleteQuestion(questionId);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['lesson-questions'] });
+            queryClient.invalidateQueries({ queryKey: ['my-questions'] });
+            toast.success(
+                'Xóa câu hỏi thành công',
+                'Câu hỏi của bạn đã được xóa thành công'
+            );
+        },
+        onError: () => {
+            toast.errorGeneral();
+        },
+    });
 };
